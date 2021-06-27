15fc2e94034562dca7d56bc56f09ce54aaf0eff8
António Alvarenga <aalvarenga@imaginarycloud.com>
2021-06-27 23:46:13 +0100

    Add Fragments, Portals and Refs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,71 +1,71 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import Modal from "../UI/Modal/Modal";
 import styles from "./Form.module.scss";
 
-const Form = ({onAddUser}) => {
-    const [name, setName] = useState("")
-    const [age, setAge] = useState("")
-    const [errorMessage, setErrorMessage] = useState()
+const Form = ({ onAddUser }) => {
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
-    const nameChangeHandler = (e) => {
-        setName(e.target.value)
-    };
+    const [errorMessage, setErrorMessage] = useState();
 
-    const ageChangeHandler = (e) => {
-        setAge(e.target.value)
-    };
-
-    const isFormValid = () => {
+    const isFormValid = (name, age) => {
         if (name && age) {
             if (age > 0) {
-                setErrorMessage(null)
-                return true
+                setErrorMessage(null);
+                return true;
             } else {
-                setErrorMessage("Please enter a valid age (> 0)")
-                return false
+                setErrorMessage("Please enter a valid age (> 0)");
+                return false;
             }
         } else {
-            setErrorMessage("Please enter a valid name and age (non-empty values)")
-            return false
+            setErrorMessage(
+                "Please enter a valid name and age (non-empty values)"
+            );
+            return false;
         }
-    }
+    };
 
     const submitHandler = (e) => {
         e.preventDefault();
-        if (isFormValid()) {
-            onAddUser({name, age})
-            setName("")
-            setAge("")
+        const name = nameInputRef.current.value
+        const age = ageInputRef.current.value
+        if (isFormValid(name, age)) {
+            onAddUser({ name, age });
+            nameInputRef.current.value = ''
+            ageInputRef.current.value = ''
         }
     };
 
     const onModalCloseHandler = () => {
-        setErrorMessage(null)
-    }
+        setErrorMessage(null);
+    };
 
     return (
         <Card>
             <form onSubmit={submitHandler}>
                 <div className={styles.formControl}>
                     <label>Username</label>
-                    <input type="text" value={name} onChange={nameChangeHandler} />
+                    <input
+                        ref={nameInputRef}
+                        type="text"
+                    />
                 </div>
                 <div className={styles.formControl}>
                     <label>Age (Years)</label>
-                    <input 
+                    <input
+                        ref={ageInputRef}
                         type="number"
-                        value={age}
-                        onChange={ageChangeHandler}/>
+                    />
                 </div>
                 <Button type="submit">Add User</Button>
             </form>
-            {errorMessage && 
+            {errorMessage && (
                 <Modal header="Invalid Input" onClose={onModalCloseHandler}>
                     {errorMessage}
                 </Modal>
-            }
+            )}
         </Card>
     );
 };
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,35 @@
 import styles from "./Modal.module.scss";
 import Button from "../Button/Button";
+import ReactDOM from "react-dom";
 
-const Modal = ({ header, children, onClose }) => {
+const Backdrop = ({ onClose }) => {
+    return <div className={styles.backdrop} onClick={onClose}></div>;
+};
+
+const ModalOverlay = ({ header, children, onClose }) => {
     return (
-        <div>
-            <div className={styles.backdrop} onClick={onClose}></div>
-            <div className={styles.modal}>
-                <h2 className={styles.header}>{header}</h2>
-                <div className={styles.children}>{children}</div>
-                <Button type="button" onClick={onClose}>
-                    Okay
-                </Button>
-            </div>
+        <div className={styles.modal}>
+            <h2 className={styles.header}>{header}</h2>
+            <div className={styles.children}>{children}</div>
+            <Button type="button" onClick={onClose}>
+                Okay
+            </Button>
         </div>
+    );
+};
+
+const Modal = ({ onClose, header, children }) => {
+    return (
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop onClose={onClose} />,
+                document.getElementById("backdrop-root")
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay onClose={onClose} header={header} children={children} />,
+                document.getElementById("overlay-root")
+            )}
+        </>
     );
 };
 
